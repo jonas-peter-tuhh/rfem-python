8e410c26d5421698b266026a5d8ee3efce39ae14
FrankFaulstich <36745927+FrankFaulstich@users.noreply.github.com>
2022-08-01 17:42:26 +0200

    Knotenlager 1 korrigiert, Analyseeinstellungen an die richtige Stelle verschoben

--- src/main.py
+++ src/main.py
@@ -46,11 +46,20 @@ Material(1, 'S235')
 Member(no=1, start_node_no=1, end_node_no=2, rotation_angle=0.0, start_section_no=1, end_section_no=1)
 
 # Schritt 7 Knotenlager
-NodalSupport(1, '1', NodalSupportType.HINGED)
+#NodalSupport(1, '1', NodalSupportType.HINGED)
+# Die Lagerung des Modells war instabil. Der Balken konnte sich frei um die X-Achse drehen.
+# Mit dem folgenden Lager wird die Verdrehung um X gehalten:
+NodalSupport(1, '1', [inf,inf,inf,inf,0,0])
+# Die ersten drei Werte in der Liste sind die Verschiebungen, die letzten drei die Verdrehungen. 'inf' bedeutet unendlich, also gehalten.
+
 NodalSupport(2, '2', NodalSupportType.ROLLER_IN_X)
 
+# Schritt 10 Analyseeinstellungen
+StaticAnalysisSettings(1, '1. Ordnung', StaticAnalysisType.GEOMETRICALLY_LINEAR)
+
 # Schritt 8 Lastfall
-LoadCase.StaticAnalysis(1, 'LF1', [False])
+#LoadCase.StaticAnalysis(1, 'LF1', [False])
+LoadCase(1, 'LF1', [False])
 
 # Schritt 9 Knotenlast
 f = 5
@@ -61,9 +70,6 @@ NodalLoad(no=1,
          magnitude=f*1000)
 
 #MemberLoad(1, magnitude=200)
-
-# Schritt 10 Analyseeinstellungen
-StaticAnalysisSettings(1, '1. Ordnung', StaticAnalysisType.GEOMETRICALLY_LINEAR)
 
 # Schritt 10 Berechnung
 Calculate_all()
