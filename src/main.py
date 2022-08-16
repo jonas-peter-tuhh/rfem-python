0fcc58182116feb4cab5135350f2c7618024e217
jonas <jonas.peter@tuhh.de>
2022-08-16 14:04:40 +0200

    Forloop läuft

--- src/main.py
+++ src/main.py
@@ -22,6 +22,7 @@ from RFEM.BasicObjects.section import Section
 from RFEM.BasicObjects.member import Member
 from RFEM.TypesForNodes.nodalSupport import *
 from RFEM.Loads.lineLoad import *
+from RFEM.Loads.memberLoad import *
 from RFEM.Loads.nodalLoad import *
 from RFEM.LoadCasesAndCombinations.staticAnalysisSettings import *
 from RFEM.LoadCasesAndCombinations.loadCase import *
@@ -30,46 +31,54 @@ from RFEM.LoadCasesAndCombinations.loadCase import *
 Model(new_model=True, model_name='Einfeldträger Bernoulli')
 Model.clientModel.service.begin_modification('new')
 
+#Vorschleife
+for i in range(5):
+
 # Schritt 3 Knoten
-Node(no=1, coordinate_X=0, coordinate_Y=0, coordinate_Z=0)
-length = 5.
-Node(no=2, coordinate_X=3, coordinate_Y=0, coordinate_Z=0)
+    Node(no=2*i+1, coordinate_X=0, coordinate_Y=i, coordinate_Z=0)
+    length = 5.
+    Node(no=2*i+2, coordinate_X=5, coordinate_Y=i, coordinate_Z=0)
 
 # Schritt 4 Querschnitt
-Section(1, 'IPE 200')
+    Section(i+1, 'IPE 200')
 
 # Schritt 5 Material
-Material(1, 'S235')
+    Material(i+1, 'S235')
 
 # Schritt 6 Balken
-Member(no=1, start_node_no=1, end_node_no=2, rotation_angle=0.0, start_section_no=1, end_section_no=1)
+    Member(no=i+1, start_node_no=2*i+1, end_node_no=2*i+2, rotation_angle=0.0, start_section_no=i+1, end_section_no=i+1)
 
 # Schritt 7 Knotenlager
+    a=int(2*i+1)
 # NodalSupport(1, '1', NodalSupportType.HINGED)
 # Die Lagerung des Modells war instabil. Der Balken konnte sich frei um die X-Achse drehen.
 # Mit dem folgenden Lager wird die Verdrehung um X gehalten:
-NodalSupport(1, '1', [inf, inf, inf, inf, 0, 0])
+    NodalSupport(2*i+1, str(a), [inf, inf, inf, inf, 0, 0])
 # Die ersten drei Werte in der Liste sind die Verschiebungen, die letzten drei die Verdrehungen. 'inf' bedeutet
 # unendlich, also gehalten.
-
-NodalSupport(2, '2', NodalSupportType.ROLLER_IN_X)
+    b=int(2*i+2)
+    NodalSupport(2*i+2, str(b), NodalSupportType.ROLLER_IN_X)
 
 # Schritt 10 Analyseeinstellungen
-StaticAnalysisSettings(1, '1. Ordnung', StaticAnalysisType.GEOMETRICALLY_LINEAR)
+    StaticAnalysisSettings(1, '1. Ordnung', StaticAnalysisType.GEOMETRICALLY_LINEAR)
 
 # Schritt 8 Lastfall
 # LoadCase.StaticAnalysis(1, 'LF1', [False])
-LoadCase(1, 'LF1', [False])
+    LoadCase(1, 'LF1', [False])
 
-# Schritt 9 Knotenlast
-f = 5
-NodalLoad(no=1,
-          load_case_no=1,
-          nodes_no='2',
-          load_direction=LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W,
-          magnitude=f * 1000)
-
-# MemberLoad(1, magnitude=200)
+# Schritt 9 Linienlast
+    f = 5
+# NodalLoad(no=1,
+#          load_case_no=1,
+#          nodes_no='2',
+#          load_direction=LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W,
+#          magnitude=f * 1000)
+    MemberLoad(no=i+1,
+               load_case_no=1,
+               members_no=str(i+1),
+    #           load_direction=LoadDirectionType.LOAD_DIRECTION_GLOBAL_Z_OR_USER_DEFINED_W,
+               magnitude=f * 1000)
+    # MemberLoad(1, magnitude=200)
 
 # Schritt 10 Berechnung
 Calculate_all()
